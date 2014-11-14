9252e49667429c513883726a65d0124913d6fb94
Zach Tellman <ztellman@gmail.com>
2014-11-14 11:53:09 -0800

    Update project.clj

--- riffle-hadoop/project.clj
+++ riffle-hadoop/project.clj
@@ -3,12 +3,12 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]
+  :dependencies [[org.clojure/clojure "1.7.0-alpha3"]
                  [byte-transforms "0.1.3"]
-                 [byte-streams "0.1.12-SNAPSHOT"]
+                 [byte-streams "0.2.0-alpha4"]
                  [org.clojure/tools.cli "0.3.1"]
                  [factual/riffle "0.1.1"]
-                 [org.clojure/tools.logging "0.3.0"]]
+                 [org.clojure/tools.logging "0.3.1"]]
   :profiles {:provided {:dependencies [[org.apache.hadoop/hadoop-client "2.2.0"]]}
              :uberjar {:aot [riffle.hadoop.cli]}}
   :main riffle.hadoop.cli
