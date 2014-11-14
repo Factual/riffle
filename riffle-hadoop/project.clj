105eff96518ffce23e6b059da2217c746d4d3482
Zach Tellman <ztellman@gmail.com>
2014-11-14 11:52:29 -0800

    Update project.clj

--- riffle-hadoop/project.clj
+++ riffle-hadoop/project.clj
@@ -7,7 +7,7 @@
                  [byte-transforms "0.1.3"]
                  [byte-streams "0.1.12-SNAPSHOT"]
                  [org.clojure/tools.cli "0.3.1"]
-                 [factual/riffle "0.1.0-SNAPSHOT"]
+                 [factual/riffle "0.1.1"]
                  [org.clojure/tools.logging "0.3.0"]]
   :profiles {:provided {:dependencies [[org.apache.hadoop/hadoop-client "2.2.0"]]}
              :uberjar {:aot [riffle.hadoop.cli]}}
