0430cc7e3824ea81e8061f3a9f4730e818be2261
Zach Tellman <ztellman@gmail.com>
2014-11-14 16:41:16 -0800

    no need for a snapshot anymore

--- riffle-hadoop/project.clj
+++ riffle-hadoop/project.clj
@@ -1,4 +1,4 @@
-(defproject factual/riffle-hadoop "0.1.0-SNAPSHOT"
+(defproject factual/riffle-hadoop "0.1.0"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
