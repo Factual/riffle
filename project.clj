facbb2957a2b83ecedce299d97657b647c6cd538
Michal Lopuszynski <lopusz@users.noreply.github.com>
2016-03-23 16:23:46 +0100

    mark 0.1.4

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject factual/riffle "0.1.3"
+(defproject factual/riffle "0.1.4"
   :description "a write-once key/value store"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
